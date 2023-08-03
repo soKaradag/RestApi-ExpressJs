45348ce9ded9f9feb8d663a2c18dcfcd9c8bc526
serdar <sok.karadag@gmail.com>
2023-08-03 16:02:56 +0300

    Created posts route file

--- security/apiKey.js
+++ security/apiKey.js
@@ -4,11 +4,12 @@ const config = require('../config');
 function verifyApiKey(req, res, next) {
   const apiKeyHeader = req.headers["api-key"];
 
+  //If api key and user api key are note same print error
   if (!apiKeyHeader || apiKeyHeader !== config.apiKey) {
     console.error("Invalid API key");
     return res.status(403).json({ error: "UNAUTHORIZED API KEY" });
   }
-
+  //Else go to next task
   next();
 }
 
